c3108e8d2077f958a35e200df69567fd67582da0
bandeira13 <malubsouza@yahoo.com.br>
2025-12-17 13:52:24 -0300

    chore: configuração inicial do projeto com Spring Data JPA

---
+++ src/main/java/br/com/dio/board_tarefas_jpa/BoardTarefasJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.board_tarefas_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTarefasJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardTarefasJpaApplication.class, args);
+	}
+
+}
