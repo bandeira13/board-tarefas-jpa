bbaded4c1fca341754eb200caaf3c9fe79a883ce
bandeira13 <malubsouza@yahoo.com.br>
2025-12-18 14:07:45 -0300

    feat: implementação da entidade Board com construtores e campos

--- src/main/java/br/com/dio/board_tarefas_jpa/model/Board.java
+++ src/main/java/br/com/dio/board_tarefas_jpa/model/Board.java
@@ -1,4 +1,19 @@
 package br.com.dio.board_tarefas_jpa.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "boards")
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
